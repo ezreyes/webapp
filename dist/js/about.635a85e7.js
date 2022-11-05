347c84352a41555fa0682f4e7868107f03ebdb1d
pat <patnacrab008@gmail.com>
2022-11-05 15:20:26 +0700

    adding dist subtree

---
+++ dist/js/about.635a85e7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var s=a("7a23");const c={class:"about"},e=Object(s["g"])("h1",null,"This is an about page",-1),o=[e];function u(n,t){return Object(s["s"])(),Object(s["f"])("div",c,o)}var b=a("6b0d"),i=a.n(b);const r={},d=i()(r,[["render",u]]);t["default"]=d}}]);
+//# sourceMappingURL=about.635a85e7.js.map
